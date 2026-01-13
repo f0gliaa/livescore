a1c496cd97f44eaf84a26161f55d42c5f51c0b38
f0gliaa <matteo.fogliani@fermi.mo.it>
2026-01-13 11:57:04 +0100

    Add files via upload
    terzo upload

--- repository/Publisher.py
+++ repository/Publisher.py
@@ -1,4 +1,87 @@
-import pymongo
-from db import database
+import datetime
 
-#db= [  ]
+import pymongo
+import time
+import random
+
+# codice per creare/accedere al database
+from pymongo import AsyncMongoClient
+
+from db import database,in_corso,programmati,terminati
+
+
+# db
+# collections  : "progammate"  "in corso"  "finite"
+# { "sport" : "calcio", "squadre" : "k vs z" ,"punti":punti,"ammonizioni":ammonizioni,"interruzioni":interruzioni}
+t=0
+
+def tempo():
+    s=0
+    m=0
+    while True:
+        s +=1
+        if s>=60:
+            s=0
+            m+=1
+        return f"{m} : {s}"
+
+
+
+
+lista_programmati = []
+
+
+class Sposta_Match():
+    def __init__(self):
+        self.db = database
+
+    def run(self):
+        while True:
+            for partita in lista_programmati:
+                if partita["ora inizo"] == datetime.datetime.now():
+                    partita.start()
+
+
+class Partita():
+    id = 0
+
+    def __init__(self, sport, squadre):
+        self.sport = sport
+        self.squadre = squadre
+        self.id = self.__class__.id
+
+    def start(self):
+        tempo = 0
+        punti = 0
+        ammonizioni = 0
+        interruzioni = 0
+        #
+        partita = {"id": self.id, "sport": self.sport, "punti": punti, "ammonizioni": ammonizioni,
+                   "interruzioni": interruzioni}
+
+        while tempo < 60:
+
+            if not xxx:
+                continue
+
+            # azioni
+            scelta = random.randint(0, 1)  # scelta se aumentare qualcosa o no
+            if scelta == 1:  # se viene aumentato qualcosa
+                r = random.randint(1, 3)  # quale valore scegliere da aumentare
+                if r == 1:
+                    partita["punti"] += 1
+                if r == 2:
+                    partita["ammonizioni"] += 1
+                if r == 3:
+                    partita["interruzioni"] += 1
+
+            await in_corso.update_one({"id": self.id}, )
+            # aumenta tempo
+            time.sleep(1)  # aspetta un secondo
+            tempo += 1
+
+
+partita = Partita()
+lista_programmati.append(partita)
+
+ora_inizio = d1 + datetime.timedelta(days=rnd.randint(1, 4))
--- repository/db.py
+++ repository/db.py
@@ -1,11 +1,52 @@
 from pymongo import AsyncMongoClient
 
+
 MONGO_URL = "mongodb://localhost:27017"
 DB_NAME = "db_score"
 
-PORT = 8888
+client = AsyncMongoClient(MONGO_URL)  # creo il client
+database = client[DB_NAME]  # creo database collegato al client
 
-client = AsyncMongoClient(MONGO_URL) #creo il client
+# creazione collections
+programmati = database["programmati"]
+in_corso = database["in_corso"]
+terminati = database["terminati"]
 
-database = client[DB_NAME] # creo database collegato al client
+#PARTE DI CREARE DEI MATCH
+
+import datetime
+import random as rnd
+
+squadre=["modena","roma","milano","cesena","torino","empoli","bologna","napoli","palermo","inter"]
+sport=["calcio","tennis","basket","volano","lotta-greco-romana"]
+
+def crea_partite():
+    lista=[]
+    for i in range(5):
+
+        prima_squadra=rnd.choice(squadre)
+        seconda_squadra=rnd.choice(squadre)
+
+        while not prima_squadra == seconda_squadra:
+            seconda_squadra=rnd.choice(squadre)
+
+        p={"ora_inizio": datetime.timedelta(seconds=rnd.randint(10,60)),"sport" : rnd.choice(sport), "squadre" : f"{prima_squadra} vs {seconda_squadra}" ,"punti":0,"ammonizioni":0,"interruzioni":0}
+        lista.append(p)
+    return lista
+
+#partite da mettere nei progammati
+#p1={"ora_inizio": 5,"sport" : "calcio", "squadre" : "k vs z" ,"punti":0,"ammonizioni":0,"interruzioni":0}
+
+
+# await collection.insert_one({"nome": "Mario", "citta": "Roma"})
+
+lista_match=crea_partite()
+
+async def inserisci_match(lista_match):
+    for p in lista_match:
+        await programmati.insert_one(p)
+
+if __name__=="__main__":
+    print(lista_match)
+    inserisci_match(lista_match)
 
